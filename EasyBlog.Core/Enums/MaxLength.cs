924944c916203d232695d608a62967adc2808887
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-01 02:14:28 +0300

    Max Değer Enum yapısı eklendi

---
+++ EasyBlog.Core/Enums/MaxLength.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyBlog.Core.Enums;
+
+public enum MaxLength
+{
+    Tiny = 10,
+    Small = 25,
+    MedimSmall = 50,
+    Medium = 100,
+    Long = 200,
+    ExtraLong = 500,
+    VeryLong = 750,
+    MaximumLong = 1000,
+    HugeLong = 5000
+}
