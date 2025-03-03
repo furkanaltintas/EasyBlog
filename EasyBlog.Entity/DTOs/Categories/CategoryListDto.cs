89a183557d48bc42bbe509bf2d05454d4872eddc
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-03 17:48:44 +0300

    Category işlemleri yapıldı.

---
+++ EasyBlog.Entity/DTOs/Categories/CategoryListDto.cs
@@ -0,0 +1,12 @@
+﻿using EasyBlog.Core.Entities.Abstract;
+
+namespace EasyBlog.Entity.DTOs.Categories;
+
+public class CategoryListDto : IDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ EasyBlog.Web/Areas/Management/Controllers/CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using EasyBlog.Core.Utilities.Results.ComplexTypes;
+using EasyBlog.Entity.DTOs.Categories;
+using EasyBlog.Service.Services.Managers;
+using EasyBlog.Web.Constants;
+using EasyBlog.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+
+namespace EasyBlog.Web.Areas.Management.Controllers;
+
+[Route(RouteConstants.Category)]
+public class CategoryController : BaseController
+{
+    private readonly IToastNotification _toastNotification;
+    public CategoryController(IBaseService serviceManager, IToastNotification toastNotification) : base(serviceManager)
+    {
+        _toastNotification = toastNotification;
+    }
+
+
+    public async Task<IActionResult> Index()
+    {
+        var dataResult = await _serviceManager.CategoryService.GetAllCategoriesNonDeletedAsync();
+
+        return View(dataResult.Data);
+    }
+
+
+    [Route(RouteConstants.Add)]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost(RouteConstants.Add)]
+    public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
+    {
+        var result = await _serviceManager.CategoryService.CreateCategoryAsync(categoryAddDto);
+
+        if (result.ResultStatus == ResultStatus.Success)
+        {
+            NotificationHelper.ShowSuccess(_toastNotification, result.Message);
+            return RedirectToAction(nameof(Index));
+        }
+
+        NotificationHelper.ShowError(_toastNotification, result.Message);
+        return View(categoryAddDto);
+    }
+
+
+    [Route(RouteConstants.Update + "/{categoryId:guid}")]
+
+    public async Task<IActionResult> Update(Guid categoryId)
+    {
+        var dataResult = await _serviceManager.CategoryService.GetCategoryByUpdateGuid(categoryId);
+
+        if (dataResult.ResultStatus == ResultStatus.Success)
+            return View(dataResult.Data);
+
+        NotificationHelper.ShowError(_toastNotification, dataResult.Message);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost(RouteConstants.Update + "/{categoryId:guid}")]
+    public async Task<IActionResult> Update(CategoryUpdateDto categoryUpdateDto, Guid categoryId)
+    {
+        var dataResult = await _serviceManager.CategoryService.UpdateCategoryAsync(categoryUpdateDto, categoryId);
+
+        if (dataResult.ResultStatus == ResultStatus.Success)
+        {
+            NotificationHelper.ShowSuccess(_toastNotification, dataResult.Message);
+            return RedirectToAction(nameof(Index));
+        }
+
+        NotificationHelper.ShowError(_toastNotification, dataResult.Message);
+        return View(categoryUpdateDto);
+    }
+
+    [Route(RouteConstants.Delete)]
+    public async Task<IActionResult> Delete(Guid categoryId)
+    {
+        var result = await _serviceManager.CategoryService.SafeDeleteCategoryAsync(categoryId);
+
+        if (result.ResultStatus == ResultStatus.Success)
+        {
+            NotificationHelper.ShowSuccess(_toastNotification, result.Message);
+            return RedirectToAction(nameof(Index));
+        }
+
+        NotificationHelper.ShowError(_toastNotification, result.Message);
+        return RedirectToAction(nameof(Index));
+    }
+}
