3c124dc495c53161e45aefeee3851323b0712c02
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-05 18:51:17 +0300

    UI kısmı için içerik listeleme, tek bir içerik getirme, pagination ve arama kısmı kodları yazıldı. UI kısmı sisteme yüklendi

--- EasyBlog.Web/Controllers/HomeController.cs
+++ EasyBlog.Web/Controllers/HomeController.cs
@@ -1,11 +1,46 @@
+using EasyBlog.Core.Utilities.Results.ComplexTypes;
+using EasyBlog.Service.Services.Managers;
+using EasyBlog.Web.Areas.Management.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace EasyBlog.Web.Controllers;
 
 public class HomeController : Controller
 {
-    public async Task<IActionResult> Index()
+    private IBaseService _serviceManager;
+
+    public HomeController(IBaseService serviceManager)
     {
-        return View();
+        _serviceManager = serviceManager;
+    }
+
+    public IActionResult Index(Guid? categoryId, int currentPage = 1, int pageSize = 3, bool isAscending = false, string keyword = null)
+    {
+        var result = string.IsNullOrEmpty(keyword)
+            ? _serviceManager.ArticleService.GetAllByPaging(categoryId, currentPage, pageSize, isAscending)
+            : _serviceManager.ArticleService.Search(keyword, currentPage, pageSize, isAscending);
+
+        return View(result.Data);
+    }
+
+    public IActionResult Search(string keyword, int currentPage = 1, int pageSize = 3, bool isAscending = false)
+    {
+        return RedirectToAction(nameof(Index), new
+        {
+            categoryId = (Guid?)null,
+            currentPage,
+            pageSize,
+            isAscending,
+            keyword
+        });
+    }
+
+    public async Task<IActionResult> Detail(Guid articleId)
+    {
+        var result = await _serviceManager.ArticleService.GetArticleAsync(articleId);
+
+        if (result.ResultStatus == ResultStatus.Success) return View(result.Data);
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ EasyBlog.Web/ViewComponents/HomeCategoriesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using EasyBlog.Service.Services.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlog.Web.ViewComponents
+{
+    public class HomeCategoriesViewComponent : ViewComponent
+    {
+        private readonly IBaseService _service;
+
+        public HomeCategoriesViewComponent(IBaseService service)
+        {
+            _service = service;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await _service.CategoryService.GetAllCategoriesNonDeletedAsync();
+
+            return View(result.Data);
+        }
+    }
+}
